8065c6b7b4f57f7378f58454158d4ed12846465c
Lenz <lenz@home>
2022-04-12 14:21:48 +0200

    Basic project layout

---
+++ lib/tabulae.ex
@@ -0,0 +1,18 @@
+defmodule Tabulae do
+  @moduledoc """
+  Documentation for `Tabulae`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tabulae.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tabulae_test.exs
@@ -0,0 +1,8 @@
+defmodule TabulaeTest do
+  use ExUnit.Case
+  doctest Tabulae
+
+  test "greets the world" do
+    assert Tabulae.hello() == :world
+  end
+end
