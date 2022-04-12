dc11bbe06646bf0d0f0850b0650ea463c02132c9
Lenz <lenz@home>
2022-04-12 14:51:27 +0200

    First release made. Ready to rock.

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 Like what, an Elixir library for tables with a Latin name?
 
 
+## Use
+
+Include:
+
+        {:tabulae, "~> 0.1.0-dev"}
+
+HexDocs: https://hexdocs.pm/tabulae
+
+
+
+
+## Reference Tools
+
+* https://hexdocs.pm/elixir/1.12.3/library-guidelines.html
+
+
--- mix.exs
+++ mix.exs
@@ -1,11 +1,16 @@
 defmodule Tabulae.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/l3nz/tabulae"
+  @version "0.1.0-dev"
+
   def project do
     [
       app: :tabulae,
-      version: "0.1.0",
-      elixir: "~> 1.13",
+      version: @version,
+      description: "Like what, an Elixir library for tables with a Latin name?",
+      package: package(),
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +26,18 @@ defmodule Tabulae.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["lenz"],
+      licenses: ["EPL-2"],
+      links: %{"GitHub" => @source_url},
+      files: ~w(.formatter.exs mix.exs README.md CHANGELOG.md lib)
     ]
   end
 end
